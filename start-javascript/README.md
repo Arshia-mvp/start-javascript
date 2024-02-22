4b0d2a509475764d0fb3d1e90b642d3c4033653c
Arshia-Zahedi <158775813+Arshia-mvp@users.noreply.github.com>
2024-02-23 01:31:22 +0330

    Add files via upload

---
+++ start-javascript/README.md
@@ -0,0 +1,2 @@
+# start-javascript
+this is my website. my website is related to javascript and it is a beautiful website.
---
+++ start-javascript/js/app.js
@@ -0,0 +1,34 @@
+let a = 7;
+let b = 12;
+let c = 5;
+console.log('hello every one');
+console.log('my name is Arshia zahedi');
+console.log(a - b);
+console.log(a + b);
+console.log(a ** b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(++ a);
+console.log(++ b);
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+console.log(b > a);
+console.log(a > b);
+console.log(b - a);
+console.log(b - a ** c);
+console.log(b + a * c);
+console.log(c ** b * a);
+console.log(b > c < a);
+alert('hello');
+let userName = prompt('whats your name');
+alert('hello '+ userName);
+let userAge = prompt('how old are you');
+if(userAge < 18){
+    alert('هنوز جوونی ها');
+}
+if(userAge > 18){
+    alert('داری پیر میشی ها');
+}
+alert('لطفا وارد کنسول هم بشوید');
